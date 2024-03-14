1afc68af78475547c442430bffdee07b79211940
Gurpreet <37729999+gurpreet1998@users.noreply.github.com>
2024-03-14 01:38:56 -0700

    Latest Deployment features

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const ip = process.env.SUBSCRIBER_IP || '127.0.0.1'; // Default IP if not provided via environment variables
-const port = process.env.SUBSCRIBER_PORT || '8000'; // Default port if not provided via environment variables
+const ip = process.env.REACT_APP_SUBSCRIBER_IP || "127.0.0.1";
+const port = process.env.REACT_APP_SUBSCRIBER_PORT || "8000";
 const baseURL = `http://${ip}:${port}`;
 
 export { baseURL };
